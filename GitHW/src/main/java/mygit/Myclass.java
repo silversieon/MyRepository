eb21756b55aad2fccd61fb60df1997a378cba07c
Comeoksal <keumsiun0503@gmail.com>
2025-12-04 14:22:03 +0900

    :sparkles: Feat: sayHello

--- GitHW/src/main/java/mygit/Myclass.java
+++ GitHW/src/main/java/mygit/Myclass.java
@@ -3,5 +3,8 @@ package mygit;
 public class Myclass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println("2 Git!");
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");
 	}
 }
