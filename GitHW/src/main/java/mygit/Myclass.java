8e2f7f788a23da3da31c4badfebcdc3953875162
Comeoksal <keumsiun0503@gmail.com>
2025-12-04 14:27:10 +0900

    :sparkles: Feat: add sayBye

--- GitHW/src/main/java/mygit/Myclass.java
+++ GitHW/src/main/java/mygit/Myclass.java
@@ -3,5 +3,8 @@ package mygit;
 public class Myclass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println("Master version");
+		YourClass your = new YourClass();
+		your.sayHello("one");
 	}
 }
--- GitHW/src/main/java/mygit/YourClass.java
+++ GitHW/src/main/java/mygit/YourClass.java
@@ -4,4 +4,8 @@ class YourClass {
 	public void sayHello(String toWhom) {
 		System.out.print("Hello! " + toWhom);
 	}
+	
+	protected void sayBye(String toWhom) {
+		System.out.print("Bye! " + toWhom);
+	}
 }
