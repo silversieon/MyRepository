9a738ce1b40f286d0971c56ea766ef0482659733
Ksieon <keumsiun0503@gmail.com>
2025-12-09 19:16:27 +0900

    :sparkles: Feat: A, C (사람, 주문 클래스 코드 추가)

--- GitHW/src/mygit/A.java
+++ GitHW/src/mygit/A.java
@@ -1,14 +1,18 @@
 package mygit;
 
 public class A {
+	
+	private Integer age;
+	private String name;
 
-	public A() {
-		// TODO Auto-generated constructor stub
+	public A(Integer age, String name) {
+		this.age = age;
+		this.name = name;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		A person = new A(3, "sieon");
+		System.out.println("age: "+ person.age + "name: "+ person.name);
 	}
 
 }
--- GitHW/src/mygit/C.java
+++ GitHW/src/mygit/C.java
@@ -1,14 +1,20 @@
 package mygit;
 
 public class C {
+	
+	private Integer price;
+	private String address;
+	private Integer count;
 
-	public C() {
-		// TODO Auto-generated constructor stub
+	public C(String address, Integer price, Integer count) {
+		this.price = price;
+		this.address = address;
+		this.count = count;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		C order = new C("서울시 성북구", 15000, 3);
+		System.out.println("order1: " + order.address + ", " + order.price + ", " + order.count);
 	}
 
 }
